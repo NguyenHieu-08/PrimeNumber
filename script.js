5a4ffef0401657e2e61338990008eee999bd61ac
Hiếu Nguyễn <hieunghuu08@gmail.com>
2022-11-14 10:20:31 +0700

    Upload so nguyen to

---
+++ script.js
@@ -0,0 +1,29 @@
+function checkPrime(number) {
+    if (number < 2) {
+        return false
+    }
+
+    // for (let i = 2; i <= Math.sqrt(number); i++)
+    for (let i = 2; i <= number/2; i++) {
+        if (number % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+function printPrimeNumber() {
+    let ipNumber = Number(document.getElementById('number').value);
+    let number = '';
+    let count = 0;
+
+    for(let i = 0; i <= ipNumber; i++) {
+        if(checkPrime(i) == true) {
+            number += i;
+            count++;
+        }
+    }
+
+    document.getElementById('primenumber').innerHTML = " " + number + " ";
+    document.getElementById('count').innerHTML = count;
+}
